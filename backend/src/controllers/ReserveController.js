8558aca363f8d8a5cd39ef254be7c0e40750081e
nicolaulucas30 <nicolau.lucas04@gmail.com>
2025-09-09 22:28:29 -0300

    Enviando curso

--- backend/src/controllers/ReserveController.js
+++ backend/src/controllers/ReserveController.js
@@ -1,4 +1,6 @@
 import Reserve from "../models/Reserve";
+import House from '../models/House';
+import User from '../models/User';
 
 class ReserveController{
 
@@ -8,6 +10,20 @@ class ReserveController{
         const { house_id } = req.params;
         const { date } = req.body;
 
+        const house = await House.findById(house_id);
+        if(!house){
+            return res.status(400).json({ error: "Essa casa não existe!" });
+        }
+
+        if(house.status !== true){
+            return res.status(400).json({ error: "Solicitação indisponível!" });            
+        }
+
+        const user = await User.findById(user_id);
+
+        if(String(user._id) === String(house.user)){
+            return res.status(401).json({ error: "Reserva não permitida!" });                
+        }
 
         const reserve = await Reserve.create({
             user: user_id,
